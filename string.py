70f474dd101153ac61f13ae9d2c997605b6dcb9e
Shifa Naaz <abubush7019@gmail.com>
2026-09-25 16:05:01 +0530

    Initial Commit

---
+++ string.py
@@ -0,0 +1,8 @@
+# Take input from the user
+user_string = input("Enter a string: ")
+
+# Get the length using len()
+length = len(user_string)
+
+# Display the result
+print("The length of the string is:", length)
